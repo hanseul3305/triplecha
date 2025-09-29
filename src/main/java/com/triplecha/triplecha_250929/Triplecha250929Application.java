52b7cd5926d92dbe1e70e494d61ac35b3e474376
hanseul3305 <chahanseulit@gmail.com>
2025-09-29 15:32:58 +0900

    세팅

---
+++ src/main/java/com/triplecha/triplecha_250929/Triplecha250929Application.java
@@ -0,0 +1,13 @@
+package com.triplecha.triplecha_250929;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Triplecha250929Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Triplecha250929Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/triplecha/triplecha_250929/Triplecha250929ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triplecha.triplecha_250929;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Triplecha250929ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
